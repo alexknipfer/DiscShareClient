f84e9f23208fd0506e42029c6061c02b6d1f3e8b
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-01 19:58:38 -0500

    nav routes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 import React from 'react'
 
@@ -5,8 +6,12 @@ const Navigation = () => {
   return (
     <div>
       <Menu pointing fixed="top">
-        <Menu.Item link>Home</Menu.Item>
-        <Menu.Item link>Login</Menu.Item>
+        <Link to="/">
+          <Menu.Item link>Home</Menu.Item>
+        </Link>
+        <Link to="/login">
+          <Menu.Item link>Login</Menu.Item>
+        </Link>
       </Menu>
     </div>
   )
