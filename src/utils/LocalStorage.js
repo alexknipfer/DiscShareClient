4c6b5d6e828aa307772a4ee54093a1770f5263de
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-05 21:12:17 -0500

    load token from local storage / cookie

--- src/utils/LocalStorage.js
+++ src/utils/LocalStorage.js
@@ -3,9 +3,16 @@ import Cookies from 'js-cookie'
 export class LocalStorage {
   static saveToken(token, options) {
     if (typeof Storage !== 'undefined') {
-      localStorage.setItem('jwtToken', token)
+      localStorage.setItem('accesstoken', token)
     } else {
       Cookies.set('accesstoken', token, options)
+    }
+  }
+  static get loadToken() {
+    if (typeof Storage !== 'undefined') {
+      localStorage.getItem('accesstoken')
+    } else {
+      return Cookies.get('accesstoken')
     }
   }
 }
