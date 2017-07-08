dac681d851bbc54746ad110b0067a21606313141
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-07 20:54:48 -0500

    setup nav for user and visitor

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,29 +1,55 @@
+import React, { Component } from 'react'
+
 import { Link } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
-import React from 'react'
 
-const Navigation = () => {
-  return (
-    <div>
-      <Menu pointing inverted fixed="top" className="nav-main">
-        <Link to="/">
-          <Menu.Item link className="nav-links">
-            Home
-          </Menu.Item>
-        </Link>
-        <Link to="/register">
-          <Menu.Item link className="nav-links">
-            Register
-          </Menu.Item>
-        </Link>
-        <Link to="/login">
-          <Menu.Item link className="nav-links">
-            Login
-          </Menu.Item>
-        </Link>
-      </Menu>
-    </div>
-  )
+class Navigation extends Component {
+  render() {
+    return this.props.auth ? this.renderUserNav() : this.renderVisitorNav()
+  }
+
+  renderVisitorNav = () => {
+    return (
+      <div>
+        <Menu pointing inverted fixed="top" className="nav-main">
+          <Link to="/">
+            <Menu.Item link className="nav-links">
+              Home
+            </Menu.Item>
+          </Link>
+          <Link to="/register">
+            <Menu.Item link className="nav-links">
+              Register
+            </Menu.Item>
+          </Link>
+          <Link to="/login">
+            <Menu.Item link className="nav-links">
+              Login
+            </Menu.Item>
+          </Link>
+        </Menu>
+      </div>
+    )
+  }
+
+  renderUserNav = () => {
+    return (
+      <div>
+        <Menu pointing inverted fixed="top" className="nav-main">
+          <Link to="/">
+            <Menu.Item link className="nav-links">
+              Home
+            </Menu.Item>
+          </Link>
+          <Link to="/login">
+            <Menu.Item link className="nav-links">
+              Logout
+            </Menu.Item>
+          </Link>
+        </Menu>
+      </div>
+    )
+  }
 }
 
 export default Navigation
