ea4be9e8d60fa18c8359f12ba72509a5b8033d74
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-07 22:52:45 -0500

    initialized edit account page

---
+++ src/routes/EditAccount/EditAccount.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class EditAccount extends Component {
+  render() {
+    const { user } = this.props
+    //console.log(user)
+    return (
+      <div>
+        <h3>Edit Account</h3>
+      </div>
+    )
+  }
+}
+
+export default EditAccount
