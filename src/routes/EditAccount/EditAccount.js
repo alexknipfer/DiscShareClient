6014f385fd67049c1b3079a8f4827c1e2c492ea5
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-08 16:40:23 -0500

    edit account submission and alert handlers

--- src/routes/EditAccount/EditAccount.js
+++ src/routes/EditAccount/EditAccount.js
@@ -1,13 +1,40 @@
-import { Button, Card, Form, Grid } from 'semantic-ui-react'
+import { Button, Card, Form, Grid, Input, Message } from 'semantic-ui-react'
 import React, { Component } from 'react'
 
 import { AccountApi } from '../../lib/apis/AccountApi'
+import { LocalStorage } from '../../utils/LocalStorage'
 
 class EditAccount extends Component {
-  handleSubmit = () => {
-    const email = document.getElementById('email').value
+  state = {
+    successMessageVisible: false
+  }
 
-    AccountApi.editAccount({ email })
+  displaySuccessMessage = () => {
+    this.setState({
+      successMessageVisible: !this.state.successMessageVisible
+    })
+    setTimeout(() => {
+      this.setState({
+        successMessageVisible: !this.state.successMessageVisible
+      })
+    }, 3000)
+  }
+
+  handleSubmit = async user => {
+    const email = document.getElementById('email').value
+    const firstName = document.getElementById('firstName').value
+    const location = document.getElementById('location').value
+    const userId = user._id
+
+    const result = await AccountApi.editAccount({
+      userId,
+      email,
+      firstName,
+      location
+    })
+    const { token } = result
+    LocalStorage.saveToken(token, { path: '/', expires: 7 })
+    this.displaySuccessMessage()
   }
 
   render() {
@@ -18,49 +45,55 @@ class EditAccount extends Component {
           <Grid.Row>
             <Grid.Column mobile={16} computer={10}>
               <Card fluid className="card-padding">
-                <Form onSubmit={this.handleSubmit}>
+                <Form
+                  onSubmit={() => this.handleSubmit(user)}
+                  success={this.state.successMessageVisible}
+                >
                   <Grid>
                     <Grid.Column mobile={16} computer={16}>
                       <h3>Edit Account</h3>
                     </Grid.Column>
                     <Grid.Column mobile={16} computer={8}>
                       <Form.Field>
-                        <Form.Input label="First Name" id="firstName" />
+                        <label>First Name</label>
+                        <Input id="firstName" />
                       </Form.Field>
                     </Grid.Column>
                     <Grid.Column mobile={16} computer={8}>
                       <Form.Field>
-                        <Form.Input label="Location" id="location" />
+                        <label>Location</label>
+                        <Input id="location" />
                       </Form.Field>
                     </Grid.Column>
                     <Grid.Column mobile={16} computer={8}>
                       <Form.Field>
-                        <Form.Input
-                          label="Username"
-                          placeholder={user.username}
-                          disabled
-                        />
+                        <label>Username</label>
+                        <Input defaultValue={user.username} disabled />
                       </Form.Field>
                     </Grid.Column>
                     <Grid.Column mobile={16} computer={8}>
                       <Form.Field>
-                        <Form.Input
-                          label="Email"
-                          placeholder={user.email}
-                          id="email"
-                          required
-                        />
+                        <label>Email</label>
+                        <Input defaultValue={user.email} id="email" required />
                       </Form.Field>
                     </Grid.Column>
                     <Grid.Column mobile={16} computer={8}>
                       <Form.Field>
-                        <Form.Input label="Test Field" />
+                        <label>Test Field</label>
+                        <Input />
                       </Form.Field>
                     </Grid.Column>
                     <Grid.Column mobile={16} computer={8}>
                       <Form.Field>
-                        <Form.Input label="Test Field" />
+                        <label>Test Field</label>
+                        <Input />
                       </Form.Field>
+                    </Grid.Column>
+                    <Grid.Column mobile={16} computer={16}>
+                      <Message
+                        success
+                        header="Your profile has been updated successfully."
+                      />
                     </Grid.Column>
                     <Grid.Column>
                       <Button type="submit">Submit</Button>
