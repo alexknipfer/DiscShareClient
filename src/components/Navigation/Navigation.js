c17d7ed12f1be9e8690799eb5acf386bbeec7a8b
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-07 21:01:48 -0500

    added logout to navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
 import { Link } from 'react-router-dom'
+import { LocalStorage } from '../../utils/LocalStorage'
 import { Menu } from 'semantic-ui-react'
+import { Redirect } from 'react-router'
 
 class Navigation extends Component {
+  state = {
+    toggleRedirect: false
+  }
+
+  logout = () => {
+    LocalStorage.deleteToken()
+    this.setState({ toggleRedirect: !this.state.toggleRedirect })
+  }
+
   render() {
     return this.props.auth ? this.renderUserNav() : this.renderVisitorNav()
   }
@@ -41,12 +52,11 @@ class Navigation extends Component {
               Home
             </Menu.Item>
           </Link>
-          <Link to="/login">
-            <Menu.Item link className="nav-links">
-              Logout
-            </Menu.Item>
-          </Link>
+          <Menu.Item link className="nav-links" onClick={this.logout}>
+            Logout
+          </Menu.Item>
         </Menu>
+        {this.state.toggleRedirect && <Redirect path to="/login" />}
       </div>
     )
   }
