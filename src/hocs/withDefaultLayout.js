dfa47f32facc516d57a42ed4246eaf1c4837f750
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-10 23:18:24 -0500

    container styled component

--- src/hocs/withDefaultLayout.js
+++ src/hocs/withDefaultLayout.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react'
 
 import Navigation from '../components/Navigation/Navigation'
+import styled from 'styled-components'
 
 export default ComposedComponent => {
+  const Container = styled.div`
+    padding: 50px;
+    background-color: #c2dfe3;
+    height: 100vh;
+  `
+
   return class withDefaultLayout extends Component {
     render() {
-      console.log(this.props)
       return (
-        <div>
+        <Container>
           <Navigation {...this.props} />
           <ComposedComponent {...this.props} />
-        </div>
+        </Container>
       )
     }
   }
