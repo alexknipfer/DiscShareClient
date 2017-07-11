dd026a5cb3b7cc07aec08ccba6ffd97c8df153d1
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-10 23:18:34 -0500

    common styles for repeat styles

---
+++ src/routes/commonStyles.js
@@ -0,0 +1,11 @@
+import { Card, Grid } from 'semantic-ui-react'
+
+import styled from 'styled-components'
+
+export const CenteredGrid = styled(Grid)`
+  display: flex;
+  justify-content: center;
+`
+export const PaddedCard = styled(Card)`
+  padding: 30px !important;
+`
