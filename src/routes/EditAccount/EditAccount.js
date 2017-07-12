7f7b0dccb075144421aa590ee2324be7a4cce08a
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-11 21:17:56 -0500

    refactor to use decorators

--- src/routes/EditAccount/EditAccount.js
+++ src/routes/EditAccount/EditAccount.js
@@ -1,113 +1,109 @@
 import { Button, Form, Grid, Input, Message } from 'semantic-ui-react'
 import { CenteredGrid, PaddedCard } from '../commonStyles'
 import React, { Component } from 'react'
-import { action, extendObservable } from 'mobx'
+import { action, observable } from 'mobx'
 
 import { AccountApi } from '../../lib/apis/AccountApi'
 import { LocalStorage } from '../../utils/LocalStorage'
 import { observer } from 'mobx-react'
 
-const EditAccount = observer(
-  class EditAccount extends Component {
-    constructor() {
-      super()
-      extendObservable(this, {
-        successMessageVisible: false,
-        displaySuccessMessage: action(() => {
-          this.successMessageVisible = !this.successMessageVisible
-          setTimeout(() => {
-            this.successMessageVisible = !this.successMessageVisible
-          }, 3000)
-        })
-      })
-    }
+@observer
+class EditAccount extends Component {
+  @observable successMessageVisible = false
 
-    handleSubmit = async user => {
-      const email = document.getElementById('email').value
-      const firstName = document.getElementById('firstName').value
-      const location = document.getElementById('location').value
-      const userId = user._id
-
-      const result = await AccountApi.editAccount({
-        userId,
-        email,
-        firstName,
-        location
-      })
-      const { token } = result
-      LocalStorage.saveToken(token, { path: '/', expires: 7 })
-      this.displaySuccessMessage()
-    }
-
-    render() {
-      const { user } = this.props
-      return (
-        <CenteredGrid>
-          <Grid.Row>
-            <Grid.Column mobile={16} computer={10}>
-              <PaddedCard fluid>
-                <Form
-                  onSubmit={() => this.handleSubmit(user)}
-                  success={this.successMessageVisible}
-                >
-                  <Grid>
-                    <Grid.Column mobile={16} computer={16}>
-                      <h3>Edit Account</h3>
-                    </Grid.Column>
-                    <Grid.Column mobile={16} computer={8}>
-                      <Form.Field>
-                        <label>First Name</label>
-                        <Input id="firstName" />
-                      </Form.Field>
-                    </Grid.Column>
-                    <Grid.Column mobile={16} computer={8}>
-                      <Form.Field>
-                        <label>Location</label>
-                        <Input id="location" />
-                      </Form.Field>
-                    </Grid.Column>
-                    <Grid.Column mobile={16} computer={8}>
-                      <Form.Field>
-                        <label>Username</label>
-                        <Input defaultValue={user.username} disabled />
-                      </Form.Field>
-                    </Grid.Column>
-                    <Grid.Column mobile={16} computer={8}>
-                      <Form.Field>
-                        <label>Email</label>
-                        <Input defaultValue={user.email} id="email" required />
-                      </Form.Field>
-                    </Grid.Column>
-                    <Grid.Column mobile={16} computer={8}>
-                      <Form.Field>
-                        <label>Test Field</label>
-                        <Input />
-                      </Form.Field>
-                    </Grid.Column>
-                    <Grid.Column mobile={16} computer={8}>
-                      <Form.Field>
-                        <label>Test Field</label>
-                        <Input />
-                      </Form.Field>
-                    </Grid.Column>
-                    <Grid.Column mobile={16} computer={16}>
-                      <Message
-                        success
-                        header="Your profile has been updated successfully."
-                      />
-                    </Grid.Column>
-                    <Grid.Column>
-                      <Button type="submit">Submit</Button>
-                    </Grid.Column>
-                  </Grid>
-                </Form>
-              </PaddedCard>
-            </Grid.Column>
-          </Grid.Row>
-        </CenteredGrid>
-      )
-    }
+  @action
+  displaySuccessMessage = () => {
+    this.successMessageVisible = !this.successMessageVisible
+    setTimeout(() => {
+      this.successMessageVisible = !this.successMessageVisible
+    }, 3000)
   }
-)
+
+  handleSubmit = async user => {
+    const email = document.getElementById('email').value
+    const firstName = document.getElementById('firstName').value
+    const location = document.getElementById('location').value
+    const userId = user._id
+
+    const result = await AccountApi.editAccount({
+      userId,
+      email,
+      firstName,
+      location
+    })
+    const { token } = result
+    LocalStorage.saveToken(token, { path: '/', expires: 7 })
+    this.displaySuccessMessage()
+  }
+
+  render() {
+    const { user } = this.props
+    return (
+      <CenteredGrid>
+        <Grid.Row>
+          <Grid.Column mobile={16} computer={10}>
+            <PaddedCard fluid>
+              <Form
+                onSubmit={() => this.handleSubmit(user)}
+                success={this.successMessageVisible}
+              >
+                <Grid>
+                  <Grid.Column mobile={16} computer={16}>
+                    <h3>Edit Account</h3>
+                  </Grid.Column>
+                  <Grid.Column mobile={16} computer={8}>
+                    <Form.Field>
+                      <label>First Name</label>
+                      <Input id="firstName" />
+                    </Form.Field>
+                  </Grid.Column>
+                  <Grid.Column mobile={16} computer={8}>
+                    <Form.Field>
+                      <label>Location</label>
+                      <Input id="location" />
+                    </Form.Field>
+                  </Grid.Column>
+                  <Grid.Column mobile={16} computer={8}>
+                    <Form.Field>
+                      <label>Username</label>
+                      <Input defaultValue={user.username} disabled />
+                    </Form.Field>
+                  </Grid.Column>
+                  <Grid.Column mobile={16} computer={8}>
+                    <Form.Field>
+                      <label>Email</label>
+                      <Input defaultValue={user.email} id="email" required />
+                    </Form.Field>
+                  </Grid.Column>
+                  <Grid.Column mobile={16} computer={8}>
+                    <Form.Field>
+                      <label>Test Field</label>
+                      <Input />
+                    </Form.Field>
+                  </Grid.Column>
+                  <Grid.Column mobile={16} computer={8}>
+                    <Form.Field>
+                      <label>Test Field</label>
+                      <Input />
+                    </Form.Field>
+                  </Grid.Column>
+                  <Grid.Column mobile={16} computer={16}>
+                    <Message
+                      success
+                      header="Your profile has been updated successfully."
+                    />
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Button type="submit">Submit</Button>
+                  </Grid.Column>
+                </Grid>
+              </Form>
+            </PaddedCard>
+          </Grid.Column>
+        </Grid.Row>
+      </CenteredGrid>
+    )
+  }
+}
 
 export default EditAccount
--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,70 +1,63 @@
 import { Button, Form, Grid, Input, Message } from 'semantic-ui-react'
 import { CenteredGrid, PaddedCard } from '../commonStyles'
 import React, { Component } from 'react'
-import { action, extendObservable } from 'mobx'
+import { action, observable } from 'mobx'
 
 import { AccountApi } from '../../lib/apis/AccountApi'
 import { LocalStorage } from '../../utils/LocalStorage'
 import { observer } from 'mobx-react'
 
-const Login = observer(
-  class Login extends Component {
-    constructor(props) {
-      super(props)
-      extendObservable(this, {
-        errorMessageVisible: false,
-        errorMessage: null,
-        displayErrMessage: action(err => {
-          this.errorMessageVisible = !this.errorMessageVisible
-          this.errorMessage = err
-        })
+@observer
+class Login extends Component {
+  @observable errorMessageVisible = false
+  @observable errorMessage = null
+
+  @action
+  displayErrMessage = err => {
+    this.errorMessageVisible = !this.errorMessageVisible
+    this.errorMessage = err
+  }
+
+  handleSubmit = async () => {
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+
+    try {
+      const result = await AccountApi.loginWithUsername({
+        username,
+        password
       })
-    }
-
-    handleSubmit = async () => {
-      const username = document.getElementById('username').value
-      const password = document.getElementById('password').value
-
-      try {
-        const result = await AccountApi.loginWithUsername({
-          username,
-          password
-        })
-        const { token } = result
-        LocalStorage.saveToken(token, { path: '/', expires: 7 })
-        this.props.history.push('/')
-      } catch (error) {
-        this.displayErrMessage(error.message)
-      }
-    }
-
-    render() {
-      return (
-        <CenteredGrid>
-          <Grid.Column mobile={16} computer={5}>
-            <PaddedCard fluid>
-              <h3>Login</h3>
-              <Form
-                onSubmit={this.handleSubmit}
-                error={this.errorMessageVisible}
-              >
-                <Form.Field>
-                  <label>Username</label>
-                  <Input id="username" required />
-                </Form.Field>
-                <Form.Field>
-                  <label>Password</label>
-                  <Input id="password" type="password" required />
-                </Form.Field>
-                <Message error content={this.errorMessage} />
-                <Button type="submit">Login</Button>
-              </Form>
-            </PaddedCard>
-          </Grid.Column>
-        </CenteredGrid>
-      )
+      const { token } = result
+      LocalStorage.saveToken(token, { path: '/', expires: 7 })
+      this.props.history.push('/')
+    } catch (error) {
+      this.displayErrMessage(error.message)
     }
   }
-)
+
+  render() {
+    return (
+      <CenteredGrid>
+        <Grid.Column mobile={16} computer={5}>
+          <PaddedCard fluid>
+            <h3>Login</h3>
+            <Form onSubmit={this.handleSubmit} error={this.errorMessageVisible}>
+              <Form.Field>
+                <label>Username</label>
+                <Input id="username" required />
+              </Form.Field>
+              <Form.Field>
+                <label>Password</label>
+                <Input id="password" type="password" required />
+              </Form.Field>
+              <Message error content={this.errorMessage} />
+              <Button type="submit">Login</Button>
+            </Form>
+          </PaddedCard>
+        </Grid.Column>
+      </CenteredGrid>
+    )
+  }
+}
 
 export default Login
