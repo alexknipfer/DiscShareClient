32d7b7df956f61516494d8e7c6d69e2ef9dec33a
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-12 21:45:48 -0500

    check for production

--- src/lib/apis/AccountApi.js
+++ src/lib/apis/AccountApi.js
@@ -1,6 +1,15 @@
 import { RestClient } from '../clients/RestClient'
 
-const post = (cmd, data) => RestClient.post(`/api/account/${cmd}`, data)
+const post = (cmd, data) => {
+  if (process.env.NODE_ENV !== 'production') {
+    return RestClient.post(`/api/account/${cmd}`, data)
+  } else {
+    return RestClient.post(
+      `${process.env.REACT_APP_API}/api/account/${cmd}`,
+      data
+    )
+  }
+}
 
 export class AccountApi {
   static createAccount(data) {
