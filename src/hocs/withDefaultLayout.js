e17557dfc3c61fe9489017042b0577c9a2787044
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-17 20:23:11 -0500

    apollo provider

--- src/hocs/withDefaultLayout.js
+++ src/hocs/withDefaultLayout.js
@@ -1,3 +1,8 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface
+} from 'react-apollo'
 import React, { Component } from 'react'
 
 import Navigation from '../components/Navigation/Navigation'
@@ -6,13 +11,21 @@ import styled from 'styled-components'
 export default ComposedComponent => {
   const Container = styled.div`padding-top: 50px;`
 
+  const client = new ApolloClient({
+    networkInterface: createNetworkInterface({
+      uri: 'http://localhost:4000/graphql'
+    })
+  })
+
   return class withDefaultLayout extends Component {
     render() {
       return (
-        <Container>
-          <Navigation {...this.props} />
-          <ComposedComponent {...this.props} />
-        </Container>
+        <ApolloProvider client={client}>
+          <Container>
+            <Navigation {...this.props} />
+            <ComposedComponent {...this.props} />
+          </Container>
+        </ApolloProvider>
       )
     }
   }
