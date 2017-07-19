47187496369a73ea7b31d8de11bf423b1e4a127c
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-18 22:14:33 -0500

    get user query

---
+++ src/queries/getUser.js
@@ -0,0 +1,11 @@
+import { gql } from 'react-apollo'
+
+export default gql`
+  query getUser($accesstoken: String) {
+    getUser(accesstoken: $accesstoken) {
+      id
+      email
+      username
+    }
+  }
+`
