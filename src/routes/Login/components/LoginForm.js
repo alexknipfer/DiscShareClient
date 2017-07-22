c3ea1af9e690881089794c69f52c202dcf4a9064
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-21 23:23:59 -0500

    use form validation on login

--- src/routes/Login/components/LoginForm.js
+++ src/routes/Login/components/LoginForm.js
@@ -1,41 +1,91 @@
+import { Button, Form, Grid, Message } from 'semantic-ui-react'
 import React, { Component } from 'react'
+import { action, observable } from 'mobx'
 
+import CenteredGrid from '../../../components/CenteredGrid/CenteredGrid'
 import FormInput from '../../../utils/Forms/FormInput'
+import { LocalStorage } from '../../../utils/LocalStorage'
+import LoginMutation from '../../../mutations/login'
+import PaddedCard from '../../../components/PaddedCard/PaddedCard'
+import PropTypes from 'prop-types'
+import { graphql } from 'react-apollo'
 import { observer } from 'mobx-react'
 
 @observer
 class LoginForm extends Component {
-  submit = e => {
-    e.preventDefault()
-    this.props.onSubmit()
+  @observable errorMessageVisible = false
+  @observable errorMessage = null
+
+  @action
+  displayErrMessage = err => {
+    this.errorMessageVisible = !this.errorMessageVisible
+    this.errorMessage = err
   }
+
+  handleSubmit = async login => {
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+
+    try {
+      const user = await login(username, password)
+      LocalStorage.saveToken(user.data.login)
+      this.props.history.push('/')
+    } catch (err) {
+      const { graphQLErrors } = err
+      this.displayErrMessage(graphQLErrors[0].message)
+    }
+  }
+
   render() {
-    console.log('FORM: ', this.props.onChange)
-    const { form, onChange } = this.props
+    const { form, login, onChange } = this.props
     return (
-      <form onSubmit={this.submit}>
-        <FormInput
-          type="email"
-          value={form.fields.email.value}
-          error={form.fields.email.error}
-          onChange={onChange}
-          placeholder="email"
-        />
-        <FormInput
-          type="password"
-          value={form.fields.password.value}
-          error={form.fields.password.error}
-          onChange={onChange}
-          placeholder="password"
-        />
-        {form.meta.error &&
-          <div>
-            {form.meta.error}
-          </div>}
-        <input disabled={!form.meta.isValid} value="Continue" type="submit" />
-      </form>
+      <CenteredGrid>
+        <Grid.Column mobile={14} computer={5}>
+          <PaddedCard fluid>
+            <h3>Login</h3>
+            <Form
+              onSubmit={() => this.handleSubmit(login)}
+              error={this.errorMessageVisible}
+            >
+              <Form.Field>
+                <FormInput
+                  id="username"
+                  name="username"
+                  value={form.fields.username.value}
+                  error={form.fields.username.error}
+                  onChange={onChange}
+                  placeholder="email"
+                />
+              </Form.Field>
+              <Form.Field>
+                <FormInput
+                  id="password"
+                  type="password"
+                  name="password"
+                  value={form.fields.password.value}
+                  error={form.fields.password.error}
+                  onChange={onChange}
+                  placeholder="password"
+                />
+              </Form.Field>
+              {form.meta.error &&
+                <div>
+                  {form.meta.error}
+                </div>}
+              <Message error content={this.errorMessage} />
+              <Button disabled={!form.meta.isValid} type="submit">
+                Login
+              </Button>
+            </Form>
+          </PaddedCard>
+        </Grid.Column>
+      </CenteredGrid>
     )
   }
 }
 
-export default LoginForm
+export default graphql(LoginMutation, {
+  props: ({ mutate }) => ({
+    login: (username, password) => mutate({ variables: { username, password } })
+  })
+})(LoginForm)
