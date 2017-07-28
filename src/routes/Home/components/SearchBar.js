cd6338ccb981906e15098ce4d31240e156a05708
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-27 21:13:31 -0500

    updated location search bar

--- src/routes/Home/components/SearchBar.js
+++ src/routes/Home/components/SearchBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Geosuggest from 'react-geosuggest'
 import { Search } from 'semantic-ui-react'
+import search from './search.css'
 import styled from 'styled-components'
 
 const SearchContainer = styled.div`
@@ -24,38 +25,10 @@ const SearchWrapper = styled.div`
 
 class SearchBar extends Component {
   render() {
-    const inputStyle = {
-      position: 'relative',
-      borderWidth: 0,
-      paddingLeft: 12,
-      width: '100%',
-      height: '35px',
-      border: 0,
-      color: '#fff',
-      backgroundColor: 'transparent',
-      outline: 'none',
-      display: 'flex',
-      alignItems: 'center'
-    }
-    const suggests = {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      maxHeight: '25em',
-      padding: 0,
-      marginTop: '-1px',
-      background: '#fff',
-      border: '2px solid #267dc0',
-      borderTopWidth: 0,
-      overflowX: 'hidden',
-      overflowY: 'auto',
-      listStyle: 'none',
-      zIndex: 500
-    }
     return (
       <SearchContainer>
         <SearchWrapper>
-          <Geosuggest style={{ input: inputStyle, suggests: suggests }} />
+          <Geosuggest />
         </SearchWrapper>
       </SearchContainer>
     )
