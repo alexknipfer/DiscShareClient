fea81753e506ce2d90178870e3c71de74e8efed0
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-08-01 22:11:00 -0500

    setup location store

---
+++ src/routes/Home/stores/LocationStore.js
@@ -0,0 +1,17 @@
+import { action, observable } from 'mobx'
+
+class LocationStore {
+  @observable
+  selectedLocation = { location: { lat: 37.774929, lng: -122.419416 } }
+
+  @action
+  setLocation = location => {
+    this.selectedLocation = { ...this.selectedLocation, location }
+  }
+
+  get currentSelectedLocation() {
+    return this.selectedLocation
+  }
+}
+
+export default new LocationStore()
