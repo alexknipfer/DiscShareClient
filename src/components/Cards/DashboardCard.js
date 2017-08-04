a41b84ba864960347182f8ff351d9fd99facc05a
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-08-03 20:59:59 -0500

    dashboard styled cards

---
+++ src/components/Cards/DashboardCard.js
@@ -0,0 +1,9 @@
+import { Card } from 'semantic-ui-react'
+import React from 'react'
+import styled from 'styled-components'
+
+const DashboardCard = styled(Card)`
+  box-shadow: none !important;
+`
+
+export default DashboardCard
