496e8f6054b64f09de549c2499bc383ec1e67eb1
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-08-03 21:00:44 -0500

    begin dash layout

--- src/routes/Dashboard/Dashboard.js
+++ src/routes/Dashboard/Dashboard.js
@@ -1,7 +1,38 @@
+import {
+  CenteredColumn,
+  CenteredGrid
+} from '../../components/CenteredGrid/CenteredGrid'
+
+import DashboardCard from '../../components/Cards/DashboardCard'
+import { Grid } from 'semantic-ui-react'
 import React from 'react'
+import { sampleData } from './sampleData'
 
 const Dashboard = () => {
-  return <h3>Dashboard</h3>
+  return (
+    <CenteredGrid>
+      <Grid.Row>
+        {sampleData.map((disc, key) => {
+          return (
+            <CenteredColumn
+              key={key}
+              mobile={16}
+              computer={4}
+              style={{ marginBottom: 20 }}
+            >
+              <DashboardCard
+                image="/images/elliot.jpg"
+                header={disc.discName}
+                meta={disc.location}
+                description={disc.description}
+                color="white"
+              />
+            </CenteredColumn>
+          )
+        })}
+      </Grid.Row>
+    </CenteredGrid>
+  )
 }
 
 export default Dashboard
