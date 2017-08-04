a7f9ada03482bb7fe2445932a3adf22461409c0e
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-08-03 20:59:46 -0500

    test image and test data

---
+++ src/routes/Dashboard/sampleData.js
@@ -0,0 +1,38 @@
+export const sampleData = [
+  {
+    discName: 'Test Disc',
+    location: 'St. Louis, MO',
+    description:
+      'The disc description goes here. Please fill these details in to help describe the found disc.'
+  },
+  {
+    discName: 'Test Disc',
+    location: 'St. Louis, MO',
+    description:
+      'The disc description goes here. Please fill these details in to help describe the found disc.'
+  },
+  {
+    discName: 'Test Disc',
+    location: 'St. Louis, MO',
+    description:
+      'The disc description goes here. Please fill these details in to help describe the found disc.'
+  },
+  {
+    discName: 'Test Disc',
+    location: 'St. Louis, MO',
+    description:
+      'The disc description goes here. Please fill these details in to help describe the found disc.'
+  },
+  {
+    discName: 'Test Disc',
+    location: 'St. Louis, MO',
+    description:
+      'The disc description goes here. Please fill these details in to help describe the found disc.'
+  },
+  {
+    discName: 'Test Disc',
+    location: 'St. Louis, MO',
+    description:
+      'The disc description goes here. Please fill these details in to help describe the found disc.'
+  }
+]
