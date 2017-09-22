d87de913f3c9bbe2e2ac6239c0ca7dfee7a065a1
alexknipfer <alexanderknipfer@gmail.com>
2017-09-22 18:05:39 -0500

    Cleanup

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
+import queryString from 'query-string'
+import { Grid } from 'semantic-ui-react'
 
 import { CenteredGrid } from '../../components/CenteredGrid/CenteredGrid'
-import DiscsByLocationQuery from '../../queries/discsByLocation'
-import { Grid } from 'semantic-ui-react'
-import LocationStore from '../../stores/LocationStore'
 import SearchBar from './components/SearchBar'
-import { graphql } from 'react-apollo'
-import queryString from 'query-string'
 
 export default class Home extends Component {
   selectLocation = location => {
