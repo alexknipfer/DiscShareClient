67fbc79e7ff6fd7b8ec881f0d5722c59f9683d8d
alexknipfer <alexanderknipfer@gmail.com>
2017-09-25 21:55:12 -0500

    Added miles selector for radius

--- src/routes/Home/components/SearchBar/SearchBar.js
+++ src/routes/Home/components/SearchBar/SearchBar.js
@@ -1,13 +1,17 @@
 import './search.css'
 
 import React, { Component } from 'react'
-
+import styled from 'styled-components'
+import { observer } from 'mobx-react'
 import Geosuggest from 'react-geosuggest'
 import { geolocated } from 'react-geolocated'
-import { observer } from 'mobx-react'
-import styled from 'styled-components'
+import { Dropdown } from 'semantic-ui-react'
+import { observable, action } from 'mobx'
 
-const SearchContainer = styled.div`margin-top: 200px;`
+const SearchContainer = styled.div`
+  display: flex;
+  margin-top: 200px;
+`
 
 const SearchWrapper = styled.div`
   display: flex;
@@ -16,11 +20,35 @@ const SearchWrapper = styled.div`
   border-radius: 3px;
   background-color: hsla(0, 0%, 39%, 0.25);
   margin: 0;
-  width: 100%;
+  width: 70%;
   height: 35px;
 `
+
+const mileSelections = [
+  {
+    text: '10 miles',
+    value: 10
+  },
+  {
+    text: '20 miles',
+    value: 20
+  },
+  {
+    text: '40 miles',
+    value: 40
+  },
+  {
+    text: '60 miles',
+    value: 60
+  }
+]
+
 @observer
 class SearchBar extends Component {
+  @observable selectedMiles = 10
+
+  @action updateMiles = miles => (this.selectedMiles = miles)
+
   render() {
     const { selectLocation } = this.props
     return (
@@ -31,6 +59,24 @@ class SearchBar extends Component {
             suggestsHiddenClassName="geosuggest__suggests--hidden"
           />
         </SearchWrapper>
+        <Dropdown
+          text={`${this.selectedMiles} miles`}
+          icon="filter"
+          floating
+          labeled
+          button
+          className="icon"
+        >
+          <Dropdown.Menu>
+            {mileSelections.map(option => (
+              <Dropdown.Item
+                key={option.value}
+                onClick={() => this.updateMiles(option.value)}
+                {...option}
+              />
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
       </SearchContainer>
     )
   }
