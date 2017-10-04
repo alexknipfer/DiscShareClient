720d53dee92b27eebd2d66a0437eb100e4791822
alexknipfer <alexanderknipfer@gmail.com>
2017-10-03 21:11:08 -0500

    Load maps script on home view for location search

--- src/routes/Home/components/HomeView/HomeView.js
+++ src/routes/Home/components/HomeView/HomeView.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
+import { observable, action } from 'mobx'
+import { observer } from 'mobx-react'
 
 import { CenteredGrid } from '../../../../components/CenteredGrid'
 import { Grid } from 'semantic-ui-react'
+import CenterLoader from '../../../../components/CenteredLoader'
 import HomeStore from '../../stores/HomeStore'
 import SearchBar from '../SearchBar'
 import queryString from 'query-string'
+import GoogleMapsService from '../../../../lib/services/GoogleMapsService'
 
+@observer
 export default class Home extends Component {
+  @observable loading = true
+
+  @action
+  toggleLoader = () => {
+    this.loading = false
+  }
+
+  async componentDidMount() {
+    await GoogleMapsService.mount()
+    this.toggleLoader()
+  }
+
   selectLocation = location => {
     const { location: { lng, lat } } = location
     const { radius } = HomeStore
@@ -16,14 +33,18 @@ export default class Home extends Component {
   }
 
   render() {
-    return (
-      <CenteredGrid centered>
-        <Grid.Row>
-          <Grid.Column mobile={14} tablet={10} computer={8}>
-            <SearchBar selectLocation={this.selectLocation} />
-          </Grid.Column>
-        </Grid.Row>
-      </CenteredGrid>
-    )
+    if (this.loading) {
+      return <CenterLoader />
+    } else {
+      return (
+        <CenteredGrid centered>
+          <Grid.Row>
+            <Grid.Column mobile={14} tablet={10} computer={8}>
+              <SearchBar selectLocation={this.selectLocation} />
+            </Grid.Column>
+          </Grid.Row>
+        </CenteredGrid>
+      )
+    }
   }
 }
