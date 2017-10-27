e8d3f4c9c3216797dea05d4826f8763a894b209e
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-10-26 20:54:38 -0500

    Remove hocs

--- src/hocs/page.js
+++
@@ -1,6 +0,0 @@
-import flowRight from 'lodash/flowRight'
-import withAuth from './withAuth'
-import withDefaultLayout from './withDefaultLayout'
-import withUser from './withUser'
-
-export const pageWithUser = flowRight(withAuth, withUser, withDefaultLayout)
