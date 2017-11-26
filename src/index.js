49228fc559a5ec0faacfe687b1c23dcdb3f6ec40
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-11-25 20:38:19 -0600

    Wired up subscriptions into client

--- src/index.js
+++ src/index.js
@@ -1,23 +1,53 @@
-import {
-  ApolloClient,
-  ApolloProvider,
-  createNetworkInterface
-} from 'react-apollo'
-
+import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
+import { ApolloProvider } from 'react-apollo'
 import App from './routes/index'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getOperationAST } from 'graphql'
 import { injectGlobal } from 'styled-components'
 import registerServiceWorker from './registerServiceWorker'
 
+const httpUri =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API
+    : 'http://localhost:4000/graphql'
+
+const wsUri = 'ws://localhost:4000/subscriptions'
+
+const link = ApolloLink.split(
+  operation => {
+    const operationAST = getOperationAST(
+      operation.query,
+      operation.operationName
+    )
+    return !!operationAST && operationAST.operation === 'subscription'
+  },
+  new WebSocketLink({
+    uri: wsUri,
+    options: {
+      reconnect: true
+    }
+  }),
+  new HttpLink({ uri: httpUri })
+)
+
+console.log('REQ URL: ', httpUri)
+
 const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri:
-      process.env.NODE_ENV === 'production'
-        ? process.env.REACT_APP_API
-        : 'http://localhost:4000/graphql'
-  })
+  link,
+  cache: new InMemoryCache()
 })
+
+// const networkInterface = createNetworkInterface({
+//   uri:
+//     process.env.NODE_ENV === 'production'
+//       ? process.env.REACT_APP_API
+//       : 'http://localhost:4000/graphql'
+// })
 
 // eslint-disable-next-line
 injectGlobal`
