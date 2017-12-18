f73c2cdbf9f98083e0c68f2f3597d9412f72fa27
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-12-17 20:31:02 -0600

    Allow for directory names

--- src/lib/formatters/formatFileName.js
+++ src/lib/formatters/formatFileName.js
@@ -1,12 +1,12 @@
 import moment from 'moment'
 
-const formatFileName = filename => {
+const formatFileName = (filename, dir) => {
   const date = moment().format('YYYYMMDD')
   const randomString = Math.random()
     .toString(36)
     .substring(2, 7)
   const cleanFileName = filename.toLowerCase().replace(/[^a-z0-9]/g, '-')
-  const newFilename = `profileImages/${date}-${randomString}-${cleanFileName}`
+  const newFilename = `${dir}/${date}-${randomString}-${cleanFileName}`
   return newFilename.substring(0, 60)
 }
 
