bba5ad0565c9b4ef81327b2e080e2b641396d302
alexknipfer <alexanderknipfer@gmail.com>
2018-03-14 21:15:12 -0500

    Fix uri

--- src/utils/apollo.js
+++ src/utils/apollo.js
@@ -2,9 +2,10 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-const httpUri = process.env.NODE_ENV
-  ? process.env.REACT_APP_API
-  : 'http://localhost:4000/graphql'
+const httpUri =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API
+    : 'http://localhost:4000/graphql'
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: httpUri }),
